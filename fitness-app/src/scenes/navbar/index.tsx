66e86de678b117f5ed518e1796e91ecb9ce4e200
Eduardo <ealvarez@carminaticonsulting.com>
2023-10-23 13:37:57 -0500

    finished with nav component, moving unto modal menu

--- fitness-app/src/scenes/navbar/index.tsx
+++ fitness-app/src/scenes/navbar/index.tsx
@@ -2,15 +2,20 @@ import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/20/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link"
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 
 
 type Props = {
-    selectedPage: string;
-    setSelectedPage: (value: string) => void;
+    selectedPage: SelectedPage;
+    setSelectedPage: (value: SelectedPage) => void;
 }
 
-const Navbar = (props: Props) => {
+const Navbar = ({selectedPage, setSelectedPage}: Props) => {
     const flexBetween = "flex items-center justify-between";
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+    const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
   return (
     <nav>
         <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
@@ -19,23 +24,35 @@ const Navbar = (props: Props) => {
                 {/* LEFT SIDE OF NAV*/}
                 <img alt="logo" src={Logo} />
                 {/* RIGHT SIDE OF NAV*/}
-                <div className={`${flexBetween} w-full`}>
-                  <div className={`${flexBetween} gap-8 text-sm`}>
-                    <Link page="Home" 
-                    selectedPage={selectedPage}
-                    setSelectedPage={setSelectedPage} />
-                    <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-                    <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-                    <Link page="Contact Us" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-                  </div>
-                  <div className={`${flexBetween} gap-8`}>
-                      <p>Sign In</p>
-                      <button>Become a member</button>
-                  </div>
-                </div>    
+                { isAboveMediumScreens ?( 
+                  <div className={`${flexBetween} w-full`}>
+                    <div className={`${flexBetween} gap-8 text-sm`}>
+                      <Link page="Home" 
+                      selectedPage={selectedPage}
+                      setSelectedPage={setSelectedPage} />
+                      <Link page="Benefits" 
+                      selectedPage={selectedPage} 
+                      setSelectedPage={setSelectedPage} />
+                      <Link page="Our Classes" 
+                      selectedPage={selectedPage} 
+                      setSelectedPage={setSelectedPage} />
+                      <Link page="Contact Us" 
+                      selectedPage={selectedPage} 
+                      setSelectedPage={setSelectedPage} />
+                    </div>
+                    <div className={`${flexBetween} gap-8`}>
+                        <p>Sign In</p>
+                        <ActionButton setSelectedPage={setSelectedPage}>Become a member</ActionButton>
+                    </div>
+                  </div>    
+                ) : (<button className="rounded-full bg-secondary-500 p-2"
+                 onClick={() => {
+                  setIsMenuToggled(!isMenuToggled)
+                }}><Bars3Icon className="h-6 w-6 text-white" /></button>)}
             </div>
           </div>
         </div>
+        {/* MOBILE MENU MODAL*/}
     </nav>
   )
 }
